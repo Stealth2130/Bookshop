8f961c1a05705d503aa6240571d2b389c2a5c60a
Анастасия Клюквина <heroes10.n@mail.ru>
2024-12-15 19:31:56 +0300

    добавила eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 var path = require("path");
 
@@ -10,13 +11,22 @@ module.exports = {
     filename: "main.js",
   },
 
-  mode: "development",
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.scss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        semi: ["warn", "always"],
+        quotes: ["warn", "double"],
       },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin(),
     ],
   },
 };
